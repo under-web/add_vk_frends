1fe8f641e1ba174485e602491df050ca60f5d95d
appdev <>
2021-04-22 13:38:14 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+
+
+try:
+    # fox_options = webdriver.FirefoxOptions()
+    # set_headless = True
+    # browser = webdriver.Firefox(firefox_options=fox_options)
+    browser = webdriver.Firefox()
+    browser.get('https://vk.com/')
+    time.sleep(1)
+    phone_box = browser.find_element_by_id('index_email').send_keys('+79613890259')
+    paswd_box = browser.find_element_by_id('index_pass').send_keys('ObLaKo_=2020')
+    time.sleep(1)
+    sign_bottom = browser.find_element_by_id('index_login_button').click()
+    time.sleep(10)
+    browser.close()
+    browser.quit()
+except Exception as e:
+    print(e)
