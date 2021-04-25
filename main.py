8dfe730ad6c062c64f635e87439e2751ad976865
appdev <>
2021-04-24 22:57:19 -0400

    add TODO, random

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def sender_vk_spam():
         message_file = input('Enter path to file: ')
         iteration = input('Enter how iteration: ')
 
-
     try:
         opts = Options()
         opts.headless = True
         assert opts.headless
 
-        browser = webdriver.Firefox()
+        browser = webdriver.Firefox(options=opts)
+        # browser = webdriver.Firefox()
         browser.get('https://vk.com/')
         time.sleep(1)
         browser.find_element_by_id('index_email').send_keys(phone)
@@ -45,8 +45,11 @@ def sender_vk_spam():
         # TODO: Настроить прокси решение https://coderoad.ru/18719980/%D0%9F%D1%80%D0%BE%D0%BA%D1%81%D0%B8-Selenium-Python-Firefox
         # TODO: библиотека python прокси
         # TODO: написать автоприем друзей
+        # TODO: записать поиск боксов в отдельную функцию
         # TODO: написать автоотправку заявок в друзья
         # TODO: добавить время отправки сообщений в лог
+        # TODO: добавить отправку своих сообщений в переменно в коментарии и на стены разных групп
+        # TODO: Что то с драйвером Message: Element <div id="post_field" class="submit_post_field dark submit_post_inited"> is not clickable at point (661,537) because another element <div id="box_layer_wrap" class="scroll_fix_wrap fixed"> obscures it
         for i in range(1, iteration):
             try:
                 browser.find_element_by_xpath('//*[@id="post_field"]').click()
@@ -67,6 +70,24 @@ def sender_vk_spam():
             except selenium.common.exceptions.WebDriverException as e:
                 print('Что то с драйвером', e)
                 time.sleep(20)
+                browser.execute_script("window.open('https://vk.com/lunev_network');")
+                time.sleep(2)
+                browser.switch_to.window(browser.window_handles[2])
+                browser.find_element_by_xpath('//*[@id="post_field"]').click()
+                print('click')
+
+                time.sleep(3)
+                with open(message_file, 'r', encoding='utf=8') as txt_file:  # выбираем сообщение из файла
+                    post_message = random.choice(txt_file.readlines())
+
+                time.sleep(1)
+                browser.find_element_by_id('post_field').send_keys(post_message)
+
+                time.sleep(1)
+                browser.find_element_by_id('send_post').click()
+                print(f'{i} сообщение отправлено')
+                time.sleep(random.randint(120, 480))
+
                 continue
     except KeyboardInterrupt:
         browser.close()
@@ -80,6 +101,8 @@ def sender_vk_spam():
     finally:
         browser.close()
         browser.quit()
+
+
 def main():
     sender_vk_spam()
 
