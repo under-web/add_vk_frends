6daf459f8282cc8d26c9fae30599f429f23ea848
appdev <>
2021-04-25 16:14:06 -0400

    add post any groups

--- main.py
+++ main.py
@@ -7,38 +7,62 @@ from selenium.webdriver.firefox.options import Options
 
 def sender_vk_spam():
     global browser
+
     phone = input('Enter your phone or email: ')
     if phone == '':
         phone = ''
         password = ''
         message_file = 'message.txt'
-        iteration = 1000
+
 
     else:
         password = input('Enter your password: ')
         message_file = input('Enter path to file: ')
         iteration = input('Enter how iteration: ')
 
-    try:
-        opts = Options()
-        opts.headless = True
-        assert opts.headless
+    while True:  # зацикливаем авторизацию на случай падения selenium драйвера
+        try:
+            opts = Options()
+            opts.headless = True
+            assert opts.headless
 
-        browser = webdriver.Firefox(options=opts)
-        # browser = webdriver.Firefox()
-        browser.get('https://vk.com/')
-        time.sleep(1)
-        browser.find_element_by_id('index_email').send_keys(phone)
-        browser.find_element_by_id('index_pass').send_keys(password)
+            # browser = webdriver.Firefox(options=opts)
+            browser = webdriver.Firefox()
+            browser.get('https://vk.com/')
+            time.sleep(1)
+            browser.find_element_by_id('index_email').send_keys(phone)
+            browser.find_element_by_id('index_pass').send_keys(password)
 
-        time.sleep(1)
-        browser.find_element_by_id('index_login_button').click()
-        print('Авторизовался')
+            time.sleep(1)
+            browser.find_element_by_id('index_login_button').click()
+            print('Авторизовался')
+            time.sleep(12)
+            break
+        except Exception as err:
+            print('Проблема с авторизацией', err)
+            time.sleep(10)
+    nbr = 3
+    vkl = 1
+    while True:
+        if nbr == 8:
+            nbr = 0
+        else:
+            pass
+        vk_frend_group = ['dobav_like_repost_piar',
+                          'tomanyfriends',
+                          'dobav_v_druzya_likeme',
+                          'club39673900',
+                          'official10000friends',
+                          'spottsila',
+                          'dobav_menya_esli_xochesh',
+                          'club77713352',
+                          'kamdee',
+                          ]
 
-        time.sleep(12)
-        browser.execute_script("window.open('https://vk.com/go2friends');")
+        vkgroup = 'https://vk.com/' + vk_frend_group[nbr]
+        browser.execute_script("window.open('{}');".format(vkgroup))
         time.sleep(3)
-        browser.switch_to.window(browser.window_handles[1])
+        browser.switch_to.window(browser.window_handles[vkl])
         time.sleep(3)
         # TODO: Добавить отсчет отправленых сообщений
         # TODO: Добавить поочередное обращение к разным группам
@@ -50,57 +74,38 @@ def sender_vk_spam():
         # TODO: добавить время отправки сообщений в лог
         # TODO: добавить отправку своих сообщений в переменно в коментарии и на стены разных групп
         # TODO: Что то с драйвером Message: Element <div id="post_field" class="submit_post_field dark submit_post_inited"> is not clickable at point (661,537) because another element <div id="box_layer_wrap" class="scroll_fix_wrap fixed"> obscures it
-        for i in range(1, iteration):
-            try:
-                browser.find_element_by_xpath('//*[@id="post_field"]').click()
-                print('click')
 
-                time.sleep(3)
-                with open(message_file, 'r', encoding='utf=8') as txt_file:  # выбираем сообщение из файла
-                    post_message = random.choice(txt_file.readlines())
+        try:
+            browser.find_element_by_xpath('//*[@id="post_field"]').click()
+            print('click')
 
-                time.sleep(1)
-                browser.find_element_by_id('post_field').send_keys(post_message)
+            time.sleep(3)
+            with open(message_file, 'r', encoding='utf=8') as txt_file:  # выбираем сообщение из файла
+                post_message = random.choice(txt_file.readlines())
 
-                time.sleep(1)
-                browser.find_element_by_id('send_post').click()
-                print(f'{i} сообщение отправлено')
-                time.sleep(random.randint(120, 480))
+            time.sleep(1)
+            browser.find_element_by_id('post_field').send_keys(post_message)
 
-            except selenium.common.exceptions.WebDriverException as e:
-                print('Что то с драйвером', e)
-                time.sleep(20)
-                browser.execute_script("window.open('https://vk.com/lunev_network');")
-                time.sleep(2)
-                browser.switch_to.window(browser.window_handles[2])
-                browser.find_element_by_xpath('//*[@id="post_field"]').click()
-                print('click')
+            time.sleep(1)
+            browser.find_element_by_id('send_post').click()
+            print(f'{nbr} сообщение отправлено')
+            time.sleep(random.randint(240, 600))
+            nbr += 1
 
-                time.sleep(3)
-                with open(message_file, 'r', encoding='utf=8') as txt_file:  # выбираем сообщение из файла
-                    post_message = random.choice(txt_file.readlines())
+        except selenium.common.exceptions.WebDriverException as e:
+            print('Что то с драйвером', e)
+            nbr += 1
+            time.sleep(20)
+            continue
 
-                time.sleep(1)
-                browser.find_element_by_id('post_field').send_keys(post_message)
+        except Exception as e:
+            print('Непредвиденная ошибка', e)
+            browser.close()
+            browser.quit()
 
-                time.sleep(1)
-                browser.find_element_by_id('send_post').click()
-                print(f'{i} сообщение отправлено')
-                time.sleep(random.randint(120, 480))
-
-                continue
-    except KeyboardInterrupt:
-        browser.close()
-        browser.quit()
-        print('Программа закрыта пользователем')
-    except Exception as e:
-        print('Непредвиденная ошибка', e)
-        browser.close()
-        browser.quit()
-
-    finally:
-        browser.close()
-        browser.quit()
+        finally:
+            browser.close()
+            browser.quit()
 
 
 def main():
